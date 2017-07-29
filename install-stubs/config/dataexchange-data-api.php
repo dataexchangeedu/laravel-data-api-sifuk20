696a7a7c8f108bf71121b6b6cda66b5686538376
Jon Nicholson <macro@drjonnicholson.com>
2017-07-29 21:51:11 +0100

    Update config and readme, making it a bit clearer

--- install-stubs/config/dataexchange-data-api.php
+++ install-stubs/config/dataexchange-data-api.php
@@ -1,9 +1,12 @@
 <?php
 return [
-    // Enter the URL given in your deployment's credentials here
-    // By default this is https://uk.staging.dataexchange.education/rest/sif/requests
+    // Enter the Base URL given in your deployment's credentials here, followed by '/rest/sif/requests'
+    // For example if the base URL is https://broker.dataexchange.education then enter https://broker.dataexchange.education/rest/sif/requests
+    // If left null then the default is used (https://uk.staging.dataexchange.education/rest/sif/requests)
     'url' => null,
 
     // Enter the token given in your deployment's credentials here
+    // It is strongly advised that you store this token in your environment file and access it using the env() helper.
+    // This is to avoid putting your secure token into version control - it is your responsibility to keep this token safe!
     'token' => ''
 ];
