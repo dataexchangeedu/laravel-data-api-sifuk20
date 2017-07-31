b221dc72e5dcbc0fcd5558511634ee46e6b43c18
Jon Nicholson <macro@drjonnicholson.com>
2017-07-31 13:22:23 +0100

    merge

--- install-stubs/config/dataexchange-data-api.php
+++ install-stubs/config/dataexchange-data-api.php
@@ -2,7 +2,7 @@
 return [
     // Enter the URL given in your deployment's credentials here
     //
-    // Note: If left null then the default is used (https://uk.staging.dataexchange.education/rest/sif/requests)
+    // Note: If `null` then the default is used (https://uk.staging.dataexchange.education/rest/sif/requests)
     'url' => null,
 
     // Enter the token given in your deployment's credentials here
