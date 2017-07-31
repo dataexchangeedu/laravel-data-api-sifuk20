3c4d9d9932faf3a4b6c905817805faff80ae99f8
Jon Nicholson <macro@drjonnicholson.com>
2017-07-31 13:20:40 +0100

    Fix config comments

--- install-stubs/config/dataexchange-data-api.php
+++ install-stubs/config/dataexchange-data-api.php
@@ -1,9 +1,13 @@
 <?php
 return [
     // Enter the URL given in your deployment's credentials here
-    // By default this is https://uk.staging.dataexchange.education/rest/sif/requests
+    //
+    // Note: If left null then the default is used (https://uk.staging.dataexchange.education/rest/sif/requests)
     'url' => null,
 
     // Enter the token given in your deployment's credentials here
+    //
+    // Note: It is strongly advised you store this token in your environment file and access it using the env() helper.
+    // This saves the token going into version control - it is your responsibility to keep this token safe!
     'token' => ''
 ];
